df2d7ac1b967101fd19df22e80efa000f4725c7d
codewith-isha <codeoncanvas20@gmail.com>
2025-10-27 17:26:47 +0530

    redux

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,13 +1,13 @@
 import Image1 from "./Images/bag.jpg"
-import Image2 from "./Images/90110WL04_1.webp.jpg"
+import Image2 from "./Images/watch.jpg"
 import Image3 from "./Images/boot.avif"
 import Image4 from "./Images/cap.jpg"
 import Image5 from "./Images/dove.jpg"
 import Image6 from "./Images/headphone.jpg"
 import Image7 from "./Images/lipstick.jpg"
 import Image8 from "./Images/robot.jpg"
-import Image9 from "./Images/sandle.jfif"
-import Image10 from "./Images/shirt.webp"
+import Image9 from "./Images/sandle.jpg"
+import Image10 from "./Images/shirt.jpg"
 export const Categories = [
   "Electronics",
   "Fashion",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { Categories } from '../assets/mockData'
+import React, { useEffect } from 'react'
+import { Categories, mockData } from '../assets/mockData'
 import Heroimage from '../assets/Images/hero.png'
 import InfoSection from '../components/InfoSection'
 import CategorySection from '../components/CategorySection'
+import { setProducts } from '../redux/productSlice'
+import { useDispatch , useSelector } from 'react-redux'
+
 
 const Home = () => {
+  const dispatch = useDispatch()
+  const products = useSelector(state => state.product)
+  useEffect(()=>{
+    dispatch(setProducts(mockData))
+  },[])
   return (
     <div className='bg-white mt-2 px-3 md:px-16 lg:px-24'>
       <div className='container mx-auto py-4 flex flex-col md:flex-row space-x-2'>
@@ -33,6 +41,15 @@ const Home = () => {
           </div>
           <InfoSection/>
           <CategorySection/>
+
+          <div>
+            <h2>Top Products</h2>
+            <div>
+              {products.products.slice(0,5).map((product)=>(
+                <div>{product.name}</div>
+              ))}
+            </div>
+          </div>
       </div>
   )
 }
